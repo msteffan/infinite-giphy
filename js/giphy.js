c8f8ff1dae5c5976d31f05e2fb75cde36dafdc3e
Melissa Steffan <msteffan2012@gmail.com>
2015-08-16 18:20:09 -0400

    had to use a jquery plugin but I have the infinite gifs

--- js/giphy.js
+++ js/giphy.js
@@ -1,12 +1,48 @@
-$("button").click(getGIFs)
+var offset = 0;
+
+$("button").click(function(){
+    event.preventDefault()
+    offset = 0;
+    getGIFs();
+
+});
 
 function getGIFs() {
-    event.preventDefault()
+    // event.preventDefault()
     var query = $("input[type='search']").val()
-    console.log(query);
-    $.getJSON('http://api.giphy.com/v1/gifs/search?q='+query+'&api_key=dc6zaTOxFJmzC', function(json){
-        console.log(json);
-
+    $.getJSON('http://api.giphy.com/v1/gifs/search?q='+query+'&offset='+offset+'&api_key=dc6zaTOxFJmzC', function(json){
+        // console.log(json);
+        // var response = json.data
+        // console.log(json.data.length);
+        for (i=0; i<10; i++ ){
+            if (i==10){
+                $("body").append("<img src='"+json.data[i].images.original.url+"' class='selector'/>")
+            } else {
+                $("body").append("<img src='"+json.data[i].images.original.url+"' />")
+            }
+            offset++
+        }
     });
-    $("input[type='search']").val('')
+    // $("input[type='search']").val('')
 }
+
+// $(".selector").on('scroll',function(){
+//     $('.selector').jscroll({
+//         callback: getGIFs()
+//     });
+// })
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() == $(document).height() - $(window).height()){
+        $('.selector').jscroll({
+            callback: getGIFs()
+        });
+    }
+});
+//
+//
+// $(window).scroll(function() {
+//   if($(window).scrollTop() + $(window).height() >= $(document).height()){
+//     getGIFs();
+//   }
+// }, 150);
