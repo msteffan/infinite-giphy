9a3e14d1d1f5052f8ed7f9c4e5a6694a4998e3bf
Melissa Steffan <msteffan2012@gmail.com>
2015-08-16 18:32:16 -0400

    added reset functionality to clear existing images from page in case of new search

--- js/giphy.js
+++ js/giphy.js
@@ -1,37 +1,31 @@
 var offset = 0;
 
 $("button").click(function(){
+    reset();
     event.preventDefault()
     offset = 0;
     getGIFs();
-
 });
 
 function getGIFs() {
-    // event.preventDefault()
+    // set the value of query
     var query = $("input[type='search']").val()
+    // call the giphy api with their test key
     $.getJSON('http://api.giphy.com/v1/gifs/search?q='+query+'&offset='+offset+'&api_key=dc6zaTOxFJmzC', function(json){
-        // console.log(json);
-        // var response = json.data
-        // console.log(json.data.length);
         for (i=0; i<10; i++ ){
             if (i==10){
                 $("body").append("<img src='"+json.data[i].images.original.url+"' class='selector'/>")
             } else {
                 $("body").append("<img src='"+json.data[i].images.original.url+"' />")
             }
+            // increment the offset value so that the next batch of images loaded is the next 10 photos, not the same ones
             offset++
         }
     });
-    // $("input[type='search']").val('')
+
+
 }
-
-// $(".selector").on('scroll',function(){
-//     $('.selector').jscroll({
-//         callback: getGIFs()
-//     });
-// })
-
+// infinite scroll
 $(window).scroll(function(){
     if ($(window).scrollTop() == $(document).height() - $(window).height()){
         $('.selector').jscroll({
@@ -39,10 +33,9 @@ $(window).scroll(function(){
         });
     }
 });
-//
-//
-// $(window).scroll(function() {
-//   if($(window).scrollTop() + $(window).height() >= $(document).height()){
-//     getGIFs();
-//   }
-// }, 150);
+
+// remove all images from previous search if there's a new search
+function reset() {
+    $("img").remove();
+
+}
